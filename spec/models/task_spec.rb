a0e5caa82cd773dbd73f09bc079b55a55f5c133f
hrom <roman@alltmb.ru>
2016-01-30 12:50:48 +0300

    Models specs

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Task, :type => :model do
-  it { is_expected.to belong_to(:user) }
+  describe 'associations' do
+    it { is_expected.to belong_to(:user) }
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:state) }
+    it { is_expected.to validate_inclusion_of(:state).in_array(%w(new started finished)) }
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  it { is_expected.to have_many(:tasks) }
+  describe 'associations' do
+    it { is_expected.to have_many(:tasks) }
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_presence_of(:password) }
+  end
 end
