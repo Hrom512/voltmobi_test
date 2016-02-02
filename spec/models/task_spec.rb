87cc9cc4f41ec068fd616f51bc2ac13a9a64b0e7
hrom <roman@alltmb.ru>
2016-02-02 19:51:48 +0300

    Task state machine

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Task, :type => :model do
-  let(:task) { create(:task, state: nil) }
-
   describe 'associations' do
     it { is_expected.to belong_to(:user) }
     it { is_expected.to have_many(:attachments) }
@@ -14,7 +12,37 @@ RSpec.describe Task, :type => :model do
     it { is_expected.to validate_inclusion_of(:state).in_array(%w(new started finished)) }
   end
 
-  it 'have initial state' do
-    expect(task.state).to eq('new')
+  describe 'states' do
+    let(:task) { create(:task, state: nil) }
+
+    it 'have initial state' do
+      expect(task.state).to eq('new')
+    end
+
+    context 'with initial state' do
+      it 'can be started' do
+        expect { task.start }.to change(task, :state).to('started')
+      end
+
+      it 'can not be finished' do
+        expect { task.finish }.not_to change(task, :state)
+      end
+    end
+
+    context 'with started state' do
+      let(:task) { create(:task, state: 'started') }
+
+      it 'can be finished' do
+        expect { task.finish }.to change(task, :state).to('finished')
+      end
+    end
+
+    context 'with finished state' do
+      let(:task) { create(:task, state: 'finished') }
+
+      it 'can not be started' do
+        expect { task.start }.not_to change(task, :state)
+      end
+    end
   end
 end
